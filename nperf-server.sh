e3be0824457f97c7fb8995573deb16126ab09294
aoopa <45785074+aoopa@users.noreply.github.com>
2018-12-11 16:56:45 +0700

    Create nperf-server.sh

---
+++ nperf-server.sh
@@ -0,0 +1,19 @@
+# Compiled by Apivat Pattana-anurak 
+# Red Hat Enterprise Linux 6 64 bits / Linux 7 64 bits
+# CentOS 6 64 bits # CentOS 7 64 bits
+# CPU 2-core 1,6 GHz+ (2,4 GHz+ for 10G connections)
+# RAM 4 GB / HDD 10GB Network 1 Gbps symmetric or more
+# This can be a VM (VMWare, Xen, KVM...)
+
+yum -y install yum-utils wget &&\
+rpm --import https://repo.nperf.com/conf/nperf-server.gpg.key &&\
+wget https://repo.nperf.com/rhel/nperf-server.repo -O /tmp/nperf-server.repo &&\
+yum-config-manager --add-repo /tmp/nperf-server.repo &&\
+yum check-update
+
+# install 
+yum -y install nperf-server
+
+#StartService
+systemctl enable nperf-server
+/etc/init.d/nperf-server start
