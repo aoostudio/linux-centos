80ca0878fb061d33806a2cf9b44dcfbe802b67a3
aoopa <45785074+aoopa@users.noreply.github.com>
2018-12-18 11:10:51 +0700

    Update aoolev2.sh

--- aoolev2.sh
+++ aoolev2.sh
@@ -24,6 +24,7 @@ curl -O /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoopa/l
 chmod 600 /home/aoo/.ssh/authorized_keys
 chown aoo.aoo -R /home/aoo /home/aoo/.ssh
 
+echo -e '\e[1;36mConfig + Securing SSHD.........................................................................OK\e[0m';
 echo 'AllowUsers aoo' >> /etc/ssh/sshd_config
 sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
 
