86124c499bffe0b8cc8dbb7d1dc06a18c139a5df
aoopa <45785074+aoopa@users.noreply.github.com>
2018-12-20 19:11:22 +0700

    Update line_Mackerel.sh

--- line_Mackerel.sh
+++ line_Mackerel.sh
@@ -4,23 +4,14 @@
 # SysAdmin & Programmer
 
 
-##### Step 1 Install #####
-# Centos 6
-curl -fsSL https://mackerel.io/file/script/setup-all-yum.sh | MACKEREL_APIKEY='6PdHGkAkfTr2bX5Rma4BRi7FL3J8bmgsces8fSPj5UPD' sh
-# Chk Status Centos 6
-/var/log/mackerel-agent.log
-
-# Centos 7
-curl -fsSL https://mackerel.io/file/script/setup-all-yum-v2.sh | MACKEREL_APIKEY='6PdHGkAkfTr2bX5Rma4BRi7FL3J8bmgsces8fSPj5UPD' sh
-# Chk Status Centos 7
-sudo journalctl -u mackerel-agent.service
-
-# Debian 8 or Later
-wget -q -O - https://mackerel.io/file/script/setup-all-apt-v2.sh | MACKEREL_APIKEY='6PdHGkAkfTr2bX5Rma4BRi7FL3J8bmgsces8fSPj5UPD' sh
-# Chk Status Debian 8 or Later
-sudo journalctl -u mackerel-agent.service
-
+##### Step 1 Download #####
 #All Linux Install 
 curl -O https://mackerel.io/file/agent/tgz/mackerel-agent-latest.tar.gz
 
+tar xvzf mackerel-agent-latest.tar.gz
+rm mackerel-agent/mackerel-agent.conf 
+y
+
+curl -o mackerel-agent/mackerel-agent.conf https://raw.githubusercontent.com/aoopa/linux-centos/master/mackerel-agent.conf
+chmod 600 mackerel-agent/mackerel-agent.conf
 cd mackerel-agent && ./mackerel-agent --conf=./mackerel-agent.conf
