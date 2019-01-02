77cae2e548536f4b7cef89dddf899cd2ff5234a1
aoopa <45785074+aoopa@users.noreply.github.com>
2019-01-02 19:05:39 +0700

    Update aoo.sh

--- aoo.sh
+++ aoo.sh
@@ -3,18 +3,20 @@
 # Script Developed by Apivat Pattana-Anurak
 # SysAdmin & Programmer # Thailand # Bangkok
 
+################## Step1 basic install & update ####################
 # install
 yum install screen -y
-screen
 yum install perl -y
 yum install net-tools -y
 yum install apt -y
+yum install whois
 
 # update os
 yum -y update
 
-# Create Aoo Account
-
+################## Step2 Create Aoo Account SSH ####################
+# install sudo
+yum install sudo -y
 echo -e '\e[1;36mCreate Special User "aoo".....................................................................OK\e[0m';
 useradd -p paHW.7qDiHJCM aoo
 usermod -G wheel aoo
@@ -24,9 +26,11 @@ curl -o /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoopa/l
 chmod 600 /home/aoo/.ssh/authorized_keys
 chown aoo.aoo -R /home/aoo /home/aoo/.ssh
 
+### AllowUsers aoo 2 Sudo ###
 echo -e '\e[1;36mConfig + Securing SSHD.........................................................................OK\e[0m';
-echo 'AllowUsers aoo' >> /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
+echo 'AllowUsers aoo' >> /etc/ssh/sshd_config
+sed -i 's/# %wheel\tALL=(ALL)\tNOPASSWD: ALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/' /etc/sudoers
 
-# Disable password authentication forcing use of keys
-PasswordAuthentication no
+
