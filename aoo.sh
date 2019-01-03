da910290f16ca9b216b69b5e5ce33843287a9693
aoopa <45785074+aoopa@users.noreply.github.com>
2019-01-03 09:11:31 +0700

    Update aoo.sh
    # Aoo Basic installation wrapper # Version 1.0.0.3
    # Script Developed by Apivat Pattana-Anurak
    # SysAdmin & Programmer Form #Bangkok #Thailand

--- aoo.sh
+++ aoo.sh
@@ -39,6 +39,7 @@ sed -i 's/# %wheel\tALL=(ALL)\tNOPASSWD: ALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/' 
 ## htop procress ##
 wget dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 rpm -ihv epel-release-7-11.noarch.rpm 
+yum -y install epel-release
 yum install htop -y
 
 echo "************************************************"
